7076910648546f78ea67046f1d848e8660ee4f18
あまみや ゆうこ <1@1234.sh>
2018-01-18 08:56:43 +0800

    Implement other availability actions

--- JoyOI.TestInProduction/Controllers/AvailabilityController.cs
+++ JoyOI.TestInProduction/Controllers/AvailabilityController.cs
@@ -22,5 +22,69 @@ namespace JoyOI.TestInProduction.Controllers
                 }
             }
         }
+
+        public async Task<IActionResult> Help()
+        {
+            using (var client = new HttpClient() { BaseAddress = new Uri("http://help.joyoi.cn") })
+            using (var response = await client.GetAsync("/"))
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return Result(200, "Succeeded");
+                }
+                else
+                {
+                    return Result(500, "Failed");
+                }
+            }
+        }
+
+        public async Task<IActionResult> Forum()
+        {
+            using (var client = new HttpClient() { BaseAddress = new Uri("http://forum.joyoi.cn") })
+            using (var response = await client.GetAsync("/"))
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return Result(200, "Succeeded");
+                }
+                else
+                {
+                    return Result(500, "Failed");
+                }
+            }
+        }
+
+        public async Task<IActionResult> UserCenter()
+        {
+            using (var client = new HttpClient() { BaseAddress = new Uri("http://uc.joyoi.cn") })
+            using (var response = await client.GetAsync("/"))
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return Result(200, "Succeeded");
+                }
+                else
+                {
+                    return Result(500, "Failed");
+                }
+            }
+        }
+
+        public async Task<IActionResult> OnlineJudgeAPI()
+        {
+            using (var client = new HttpClient() { BaseAddress = new Uri("http://api.oj.joyoi.cn") })
+            using (var response = await client.GetAsync("/api/problem/a-plus-b-problem"))
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return Result(200, "Succeeded");
+                }
+                else
+                {
+                    return Result(500, "Failed");
+                }
+            }
+        }
     }
 }
