2de0df2f6c33be8b2b45673d7066bada29cce19a
あまみや ゆうこ <1@1234.sh>
2018-01-20 20:43:13 +0800

    Add icm controller

---
+++ JoyOI.TestInProduction/Controllers/IcMController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoyOI.TestInProduction.Controllers
+{
+    public class IcMController : BaseController
+    {
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
